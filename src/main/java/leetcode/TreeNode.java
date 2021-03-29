a7d5cc49affb3a96b1824143c3ea1a3845f54331
traxexee <973347676@qq.com>
2021-03-30 00:08:55 +0800

    官方解答

---
+++ src/main/java/leetcode/TreeNode.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+/**
+ * @Author: zhen
+ * @DateTime: 2021/3/28 15:15
+ * @Description: TODO
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
